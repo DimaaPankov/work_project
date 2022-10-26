1f1f67c80038b7a9114c321f3243c4af24132749
DimaaPankov <dimaray100500@gmail.com>
2022-10-26 10:14:28 +0300

    finish

--- app/src/main/java/com/mops/asois/model.kt
+++ app/src/main/java/com/mops/asois/model.kt
@@ -5,31 +5,10 @@ import android.content.SharedPreferences
 
 
 
-
-class DataBasePreferences(val _context: Context) {
-    private var prefs: SharedPreferences
-    private val prefsSetting = _context.getSharedPreferences("databaseInfo", Context.MODE_PRIVATE)
-    private var editor: SharedPreferences.Editor
-    private val editorSettings = prefsSetting.edit()
-    init {
-        prefs = _context.getSharedPreferences(getBalans().toString(), Context.MODE_PRIVATE)
-        editor = prefs.edit()
-    }
-
-    fun getBalans(): Int {
-        return prefsSetting.getInt("size",0)
-    }
-
-    fun setBalans(i:Int) {
-        editorSettings.putInt("size",i).apply()
-    }
-}
-
-
 var balance = 1000F
 var bet = 34.5F
 var x = 3.5
 var winBat = 0F
 var winOrFalse = true
 var noLouding = false
-
+/djgvifv
